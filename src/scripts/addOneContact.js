8271980fb1179b0176772c4cff62523533b5ec39
Daryna Kurytsyna <darina.kuritsina@gmail.com>
2025-01-09 11:53:26 +0100

    update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    let newContacts = [];
+    const arrayOfContacts = await readContacts();
+    const newContact = createFakeContact();
+    newContacts = [...arrayOfContacts, newContact];
+    await writeContacts(newContacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 addOneContact();
